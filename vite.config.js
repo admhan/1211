260daed170b93bbfb509401bcf569590a0a28f30
admhan <151545646+admhan@users.noreply.github.com>
2025-11-15 11:45:52 +0100

    Modify base path comment in vite.config.js
    Updated base path comment for GitHub Pages.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: './'
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/1211/",     // << obligatoire pour GitHub Pages
+  base: "/1211/",   // Chemin obligatoire pour GitHub Pages (remplace par ton repo)
   plugins: [react()],
 });
