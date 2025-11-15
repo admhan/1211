5ab47c1cc0fde275dd39607bff1c08912b04bbc2
admhan <151545646+admhan@users.noreply.github.com>
2025-11-15 11:19:25 +0100

    Change base path for GitHub Pages
    Update base path for GitHub Pages deployment.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/1211/",     // << obligatoire pour GitHub Pages
   plugins: [react()],
-  base: './'
 });
